676c6d3af50eca87a33653186570d1fb8cfb1ec8
Alisson Magalhaes <alisson08silva@gmail.com>
2021-09-17 10:51:34 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,13 @@
 import ImgDsDark from 'assets/img/ds-dark.svg';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="container">
-                <img src={ImgDsDark} alt="" width="120" height="24" />
+                <Link to="/">
+                    <img src={ImgDsDark} alt="" width="120" height="24" />
+                </Link>
             </div>
         </nav>
     );
